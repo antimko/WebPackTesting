91a9e7baa893705d2f57688851d6274696709b0e
Andrejus Timko <timcostudios@gmail.com>
2021-05-25 11:28:37 +0300

    css loader

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,10 +2,18 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
-  entry: "app.js",
+  entry: "./src/app.js",
   output: {
     path: path.resolve(__dirname, "./dist"),
     filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
